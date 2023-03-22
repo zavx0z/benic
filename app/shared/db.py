69ab57acfb950a61eed7e1a7ae02b70bebd8255e
zavx0z <metaversebdfl@gmail.com>
2023-03-22 23:44:41 +0300

    clean

--- app/shared/db.py
+++ app/shared/db.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
@@ -16,13 +15,3 @@ async def get_db():
 async def get_async_session():
     async with async_session() as session:
         yield session
-
-
-def _read_sql(con, stmt):
-    return pd.read_sql(stmt, con)
-
-
-async def get_df(stmt, index=None):
-    async with engine.begin() as conn:
-        data = await conn.run_sync(_read_sql, stmt,)
-    return data
